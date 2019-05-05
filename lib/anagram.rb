7d11d71a3c848ff412dfb9013c194b2a6eb2bb50
Unsar Malik <unsar.malik24@gmail.com>
2019-05-05 21:24:25 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+
+def initialize(word)
+  @word=word 
+end
+
+def match(some_word)
+    some_word.select do |word|
+      word.split("").sort == @word.split("").sort
+end
+end
+end
